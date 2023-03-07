386bea68d73a0395c1bc68154f6cd50417f22875
Nilupa Illangarathna <nilupalakshantha50@gmail.com>
2023-03-07 22:33:17 +0530

    all works fine, Now we have to create the proper dictorary

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,18 @@
 
 function openNav1() {
 
-  if(document.getElementById("mySidenav").style.width === "0%" && document.getElementById("main").style.marginLeft === "0%"){
-    document.getElementById("mySidenav").style.width = "40%";
-    document.getElementById("main").style.marginLeft= "40%";
+  if(document.getElementById("mySidenav1").style.width === "0%" && document.getElementById("main").style.marginLeft === "0%"){
+    document.getElementById("mySidenav1").style.width = "25%";
+    document.getElementById("main").style.marginLeft= "25%";
   }
   else{
-  document.getElementById("mySidenav").style.width = "0%";
+  document.getElementById("mySidenav1").style.width = "0%";
   document.getElementById("main").style.marginLeft = "0%";
   }
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0%";
+  document.getElementById("mySidenav1").style.width = "0%";
   document.getElementById("main").style.marginLeft= "0";
   document.getElementById("HeadingOfThePage").style.margin= "0rem 0rem 0rem 0rem";
 }
